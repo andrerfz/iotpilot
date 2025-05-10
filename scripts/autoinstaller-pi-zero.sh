77f4b192ca12ed410fc46f5c54f0e29b66de76fb
andrerfz <andre.fz@outlook.com>
2025-05-10 14:42:06 +0200

    --wip-- [skip ci]

--- scripts/autoinstaller-pi-zero.sh
+++ scripts/autoinstaller-pi-zero.sh
@@ -3,8 +3,8 @@
 # IotPilot Raspberry Pi Zero 2W Installer
 # This script installs IotPilot directly on a Raspberry Pi Zero 2W running Debian Bookworm
 # Usage:
-#   Basic: curl -sSL https://raw.githubusercontent.com/andrerfz/iotpilot/main/scripts/iotpilot-pi-zero-install.sh | sudo bash
-#   With Tailscale key: curl -sSL https://raw.githubusercontent.com/andrerfz/iotpilot/main/scripts/iotpilot-pi-zero-install.sh | sudo TAILSCALE_AUTH_KEY="tskey-auth-xxxx" bash
+#   Basic: curl -sSL https://raw.githubusercontent.com/andrerfz/iotpilot/main/scripts/autoinstaller-pi-zero.sh | sudo bash
+#   With Tailscale key: curl -sSL https://raw.githubusercontent.com/andrerfz/iotpilot/main/scripts/autoinstaller-pi-zero.sh | sudo TAILSCALE_AUTH_KEY="tskey-auth-xxxx" bash
 
 set -e
 
